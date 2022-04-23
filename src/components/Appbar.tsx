589e24d5358858bea67761425248130ce62ff414
Stefan Roeck <stefan.roeck@gmail.com>
2022-04-23 21:02:49 +0200

    convert px to em

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useBoard } from '../hooks/useBoard';
 import './appbar.css';
 import { NextPlayer } from './NextPlayer';
 
 export const Appbar = () => {
+    const { board } = useBoard();
+    const status = board.victory === undefined ? board.nextPlayer + ', your turn!' : board.victory + ', you won!';
     return (<>
         <div className='appbar-container'>
             <div>
                 <h1 className='appbar-text'>Frieda's Connect 4</h1>
                 <p className='appbar-hint'>Select a row the play a stone.</p>
+                <p className='appbar-hint'>{status}</p>
             </div>
             <NextPlayer />
         </div>
--- src/components/NextPlayer.tsx
+++ src/components/NextPlayer.tsx
@@ -6,10 +6,8 @@ import './nextPlayer.css';
 
 export const NextPlayer = () => {
     const { board } = useBoard();
-    const status = board.victory === undefined ? board.nextPlayer + ', your turn!' : board.victory + ', you won!';
 
     return (<div className='nextPlayer-container'>
-        <p className='appbar-text'>{status}</p>
         {board.victory === undefined &&
             <FieldComponent fieldState={board.nextPlayer} onSelect={doNothing} />
         }
